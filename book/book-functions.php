123f8602ff5817854d237179caa81889be50bb00
Ihor-Zaiets <dekstroer8202@gmail.com>
2022-12-07 18:08:16 +0100

    book crud success

---
+++ book/book-functions.php
@@ -0,0 +1,36 @@
+<?php
+session_start();
+$db = mysqli_connect('localhost', 'root', '', 'library_application');
+
+// initialize variables
+$name = "";
+$isbn = "";
+$id = 0;
+$update = false;
+
+if (isset($_POST['save'])) {
+    $name = $_POST['name'];
+    $isbn = $_POST['isbn'];
+
+    mysqli_query($db, "INSERT INTO book (name, isbn) VALUES ('$name', '$isbn')");
+    $_SESSION['message'] = "isbn saved";
+    header('location: books.php');
+}
+
+if (isset($_POST['update'])) {
+    echo 'update';
+    echo $id = $_POST['id'];
+    echo $name = $_POST['name'];
+    echo $isbn = $_POST['isbn'];
+
+    mysqli_query($db, "UPDATE book SET name='$name', isbn='$isbn' WHERE id=$id");
+    $_SESSION['message'] = "isbn updated!";
+    header('location: books.php');
+}
+
+if (isset($_GET['del'])) {
+    $id = $_GET['del'];
+    mysqli_query($db, "DELETE FROM book WHERE id=$id");
+    $_SESSION['message'] = "Address deleted!";
+    header('location: books.php');
+}
--- index.php
+++
@@ -1,9 +0,0 @@
-<html>
-<body>
-
-<a href="bookRepository/book-read.php">Books</a>
-<form action="theSamePage.php" method="post">
-    <input type="submit" name="someAction" value="GO" />
-</form>
-</body>
-</html>
